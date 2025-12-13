3f7aab88e7db18a65bda37230714f1a11b667d83
vantriktpm <vantriktpm@gmail.com>
2025-12-13 12:42:22 +0700

    Sửa bản build release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -15,7 +15,7 @@ android {
         minSdk = 24
         targetSdk = 36
         versionCode = 2
-        versionName = "1.0.1"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
